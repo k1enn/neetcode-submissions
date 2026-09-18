07cd43d110c2c27447359de9fc485a8793328363
CTLD.k1enn <86731193+k1enn@users.noreply.github.com>
2026-09-18 18:24:35 +0700

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+      if(nums.empty()) return {};
+
+      unordered_map<int,int> m;
+      for(int i = 0; i < nums.size(); ++i) {
+        int c = target - nums[i];
+        if (m.contains(c)) return {m[c], i};
+        m[nums[i]] = i;
+      }
+       
+        
+    }
+};
