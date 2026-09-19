a07700f5b7a09d20bcf23d0b45adb2e689a9270e
CTLD.k1enn <86731193+k1enn@users.noreply.github.com>
2026-09-20 06:49:22 +0700

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cpp
@@ -0,0 +1,11 @@
+class Solution {
+   public:
+    bool hasDuplicate(vector<int>& nums) {
+      if(nums.empty()) {
+        return false;
+      }
+
+      unordered_set<int> set (nums.begin(), nums.end());
+      return set.size() < nums.size();
+    }
+};
